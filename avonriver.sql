ce38d766c562c93e369100819951aad39d952e96
mpiko <michael@piko.com.au>
2018-12-20 08:29:59 +1100

    added

---
+++ avonriver.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS avonriver;
+CREATE DATABASE avonriver;
+
+use avonriver;
+
+CREATE TABLE riverdata (
+    date        datetime,
+    hight    float
+);
+
+DROP USER IF EXISTS 'avon'@'localhost';
+CREATE USER 'avon'@'localhost' identified by 'river';
+GRANT ALL PRIVILEGES ON avonriver.* TO 'avon'@'localhost';
+FLUSH PRIVILEGES;
